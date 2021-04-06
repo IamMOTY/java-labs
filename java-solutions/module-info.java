b39da35bb9a2e2c48a1165b5b891b517c0647140
IamMOTY <mysmplmail@gmail.com>
2021-04-06 17:52:36 +0300

    windows scripts added

---
+++ java-solutions/module-info.java
@@ -0,0 +1,6 @@
+module info.kgeorgiy.ja.buduschev.implementor {
+    requires transitive info.kgeorgiy.java.advanced.base;
+    requires info.kgeorgiy.java.advanced.implementor;
+    requires java.compiler;
+    exports info.kgeorgiy.ja.buduschev.implementor;
+}
