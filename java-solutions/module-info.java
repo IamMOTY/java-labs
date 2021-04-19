1364c86f4c44b2afe6f9fb2b0ea494ddd04d11c4
IamMOTY <mysmplmail@gmail.com>
2021-04-19 15:20:07 +0300

    2 hw fix

---
+++ java-solutions/module-info.java
@@ -0,0 +1,6 @@
+module info.kgeorgiy.ja.buduschev.implementor {
+        requires info.kgeorgiy.java.advanced.implementor;
+        requires java.compiler;
+        exports info.kgeorgiy.ja.buduschev.implementor;
+        opens info.kgeorgiy.ja.buduschev.implementor;
+}
