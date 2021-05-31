47e62304687d44030040622f0875251e14128690
IamMOTY <mysmplmail@gmail.com>
2021-05-31 14:00:38 +0300

    11 hw build fix

--- java-solutions/info/kgeorgiy/ja/buduschev/bank/build.sh
+++ java-solutions/info/kgeorgiy/ja/buduschev/bank/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 root=../../../../../../
-ls $root
 rm -rf ${root}build
 mkdir ${root}build
 build=${root}build
 lib=${root}lib
 javac -cp "$lib/hamcrest-core-1.3.jar:$lib/jsoup-1.8.1.jar:$lib/junit.jar:$lib/quickcheck-0.6.jar" -d ${build} ./*.java
-#javac -classpath $lib/* -d ${build} *.java
