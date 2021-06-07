39cf838b6937fb78ece4f44bb9268ffdac46a05f
Linn1024 <linn1024@gmail.com>
2021-06-07 14:05:14 +0300

    Checked

--- java-solutions/info/kgeorgiy/ja/buduschev/stat/runTests.sh
+++ java-solutions/info/kgeorgiy/ja/buduschev/stat/runTests.sh
@@ -4,4 +4,4 @@
 cd ../../../../../../build && \
 cp ../lib/junit.jar . && \
 lib=../lib && \
-java -cp "$lib/hamcrest-core-1.3.jar:$lib/jsoup-1.8.1.jar:$lib/junit.jar:$lib/quickcheck-0.6.jar:." info.kgeorgiy.ja.buduschev.stat.StTests
+java -cp "$lib/hamcrest-core-1.3.jar;$lib/jsoup-1.8.1.jar;$lib/junit.jar:$lib/quickcheck-0.6.jar;." info.kgeorgiy.ja.buduschev.stat.StTests
